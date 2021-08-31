1c3fa5e27f256d1ee58c3c612fc9a32bc58acb03
DigitDorian <chamacoselles@gmail.com>
2021-08-30 18:01:50 -0700

    Added Distro detection and error security in case it can not find an icon for the distro

--- Unifetch.py
+++ Unifetch.py
@@ -10,15 +10,30 @@ init()
 
 import ASCIICanvas
 
-DebianIcon = open(os.path.dirname(__file__) + "/Characters/Debian.txt", "r")
-DebianIconText = Fore.RED + DebianIcon.read()
-
+# Canvas Creation
 FetchCanvas = ASCIICanvas.Canvas(32, 9)
 FetchCanvas.Initialize()
 
-FetchCanvas.DrawString(DebianIconText, 1, 0)
+# Picking the Icon
+ComputerDistro = distro.name()
+OSColor = Fore.WHITE
 
+if ComputerDistro == "Debian GNU/Linux":
+    ComputerDistro = "Debian"
+    OSColor = Fore.RED
+elif ComputerDistro == "Linux Mint":
+    OSColor = Fore.GREEN
+else:
+    ComputerDistro = "Unknown"
+    OSColor = Fore.MAGENTA
 
+Icon = open(os.path.dirname(__file__) + "/Characters/" + ComputerDistro + ".txt", "r")
+IconText = OSColor + Icon.read()
+
+# Drawing to Canvas
+FetchCanvas.DrawString(IconText, 1, 0)
+
+# Presenting Information
 print("")
 FetchCanvas.DrawCanvas()
 print(Style.RESET_ALL + " | OS: " + distro.name() + " " + distro.version() + " | DE: " + os.environ.get('DESKTOP_SESSION'))
--- readme.txt
+++ readme.txt
@@ -1,11 +1,15 @@
 Want to make your own icons? here are the characters you can use:
 
+Important for Main Shape:
 Full Block - █
-Right - ▐
-Left - ▌
 Up - ▀
 Down - ▄
 
+Wanna Add Details?
+Right - ▐
+Left - ▌
 Corners - ▙ ▟ ▛ ▜
 Lesser Corners - ▖▗ ▘▝
+
+Shading I guess?:
 Checkered - ▚ ▞
