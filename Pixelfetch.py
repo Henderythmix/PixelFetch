8ac9b2807f9b3c6ca1edf1788d6dd48d40e6240b
DigitDorian <chamacoselles@gmail.com>
2021-08-31 16:49:20 -0700

    Draws textbox and custom text (canvas is wider), updated readme, and added some stuff to default config file

--- Pixelfetch.py
+++ Pixelfetch.py
@@ -3,6 +3,7 @@ import os
 import platform
 import distro
 import json
+import random
 
 from colorama import init
 from colorama import Fore, Back, Style
@@ -10,9 +11,18 @@ init()
 
 import ASCIICanvas
 
+# -- INITIALIZATION --
+
 # Static Variables
-FetchCanvas = ASCIICanvas.Canvas(64, 9)
+FetchCanvas = ASCIICanvas.Canvas(80, 9)
 FetchCanvas.Initialize()
+
+files = []
+
+for (dirpath, dirnames, filenames) in os.walk(os.path.dirname(__file__) + "/Quotes"):
+	files.extend(filenames)
+
+Quote = open(os.path.dirname(__file__) + "/Quotes/" + files[random.randint(-1, len(files) - 1)], "r")
 
 ConfigFile = open(os.path.dirname(__file__) + "/config.json", "r")
 Config = json.loads(ConfigFile.read())
@@ -40,25 +50,28 @@ else:
 Icon = open(os.path.dirname(__file__) + "/Icons/" + ComputerDistro + ".txt", "r")
 IconText = Icon.read()
 
-# --Drawing to Canvas--
+# -- DRAWING TO CANVAS --
 
 # Icon
 FetchCanvas.DrawString(IconText, 1, 0, Fore.RED + Style.NORMAL)
 
 # Chat Box
-FetchCanvas.ScreenData[0][18] = Fore.WHITE + Style.BRIGHT + "▗"
-FetchCanvas.ScreenData[7][18] = Fore.WHITE + Style.BRIGHT + "▝"
+FetchCanvas.ScreenData[0][18] = Fore.WHITE + Style.BRIGHT + "▛"
+FetchCanvas.ScreenData[7][18] = Fore.WHITE + Style.BRIGHT + "▙"
 for i in range(1, 7):
-    FetchCanvas.ScreenData[i][18] = Fore.WHITE + Style.BRIGHT + "▐"
-    FetchCanvas.ScreenData[i][63] = Fore.WHITE + Style.BRIGHT + "▌"
-FetchCanvas.ScreenData[0][63] = Fore.WHITE + Style.BRIGHT + "▖"
-FetchCanvas.ScreenData[7][63] = Fore.WHITE + Style.BRIGHT + "▘"
+    FetchCanvas.ScreenData[i][18] = Fore.WHITE + Style.BRIGHT + "▌"
+    FetchCanvas.ScreenData[i][79] = Fore.WHITE + Style.BRIGHT + "▐"
+FetchCanvas.ScreenData[0][79] = Fore.WHITE + Style.BRIGHT + "▜"
+FetchCanvas.ScreenData[7][79] = Fore.WHITE + Style.BRIGHT + "▟"
 
-for i in range(19, 63):
-    FetchCanvas.ScreenData[0][i] = Fore.WHITE + Style.BRIGHT + "▄"
-    FetchCanvas.ScreenData[7][i] = Fore.WHITE + Style.BRIGHT + "▀"
+for i in range(19, 79):
+    FetchCanvas.ScreenData[0][i] = Fore.WHITE + Style.BRIGHT + "▀"
+    FetchCanvas.ScreenData[7][i] = Fore.WHITE + Style.BRIGHT + "▄"
 
-# Presenting Information
+# Quote
+FetchCanvas.DrawString(Quote.read(), 19, 1, Fore.GREEN)
+
+# -- PRESENTATION --
 PCInfo = " | OS: " + distro.name(pretty=True) + EnvType + os.environ.get('DESKTOP_SESSION') + " | Shell: " + os.environ['SHELL'].split("/")[-1]
 
 print("")
---
+++ readme.md
@@ -0,0 +1,40 @@
+# PixelFetch
+
+## What is Pixel Fetch
+
+PixelFetch is a fetch program similar to Neofetch in the concept that it gathers information of your OS and shares it,
+But it is intended to be more minimal, but feel full at the same time!
+The distro icons are also intended to have a pixel art unicode style isntead of the traditional ASCII art to look more full
+
+## Running
+
+because this is programmed in python, if you want to add this to your rc file, you will have to run
+`python3 *insert full directory*/Pixelfetch.py` and the rest should run properly on its own.
+
+## Configuration
+
+You make your configurations in `config.json`. When configuring, make sure you do not destroy the config file, or else it will not work
+
+ - **Distro** *auto* - The display name for your distro (leave auto to make it decide on its own) *does not work yet*
+ - **Icon** *auto* - The icon that shows up if you want to add a custom one (leave auto to make it decide on its own) *does not work yet*
+ - **Color** *auto* - The color that shows up if you want a custom color instead (leave auto to make it decide on its own) *does not work yet*
+ - **UsesWindowManager** *false* - Set this to true if you use a window manager instead of a desktop environment
+
+### Custom Icons
+Want to make your own icons? here are the characters you can use:
+
+**Icons should be 16x8 characters. they can be smaller, but they can not be taller or wider or THEY WILL crash**
+
+Important for Main Shape:
+Full Block - █
+Up - ▀
+Down - ▄
+
+Wanna Add Details?
+Right - ▐
+Left - ▌
+Corners - ▙ ▟ ▛ ▜
+Lesser Corners - ▖▗ ▘▝
+
+Shading I guess?:
+Checkered - ▚ ▞
